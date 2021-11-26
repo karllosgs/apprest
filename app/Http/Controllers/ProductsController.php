a4160ff7cd28c3931f52e844f05bba236429f869
karllosgs <karllosgs@hotmail.com>
2021-11-26 18:09:53 -0300

    feat: CRUD products.

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        return Product::create($request->all());
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->all());
+        return $product;
+    }
+
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    public function destroy(Request $request, Product $product)
+    {
+        $product->delete();
+        return $product;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function () {
+    Route::get('/products', [ProductsController::class, 'index']);
+    Route::post('/products', [ProductsController::class, 'store']);
+    Route::put('/products/{product}', [ProductsController::class, 'update']);
+    Route::get('/products/{product}', [ProductsController::class, 'show']);
+    Route::delete('/products/{product}', [ProductsController::class, 'destroy']);
 });
